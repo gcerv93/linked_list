c3ef14a538353bec07f6b505f142de02cced6d93
gcerv93 <gcerv93@protonmail.com>
2022-01-17 10:25:46 -0600

    add list creation

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -2,14 +2,31 @@
 
 # class for the full linked list
 class LinkedList
+  attr_accessor :head, :tail
+
   def initialize
     @head = nil
     @tail = nil
+  end
+
+  def append(value)
+    node = Node.new(value)
+    tail.next_node = node unless tail.nil?
+    @tail = node
+    @head = node if head.nil?
+  end
+
+  def prepend(value)
+    node = Node.new(value)
+    node.next_node = head unless head.nil?
+    @head = node
   end
 end
 
 # class for each node
 class Node
+  attr_accessor :value, :next_node
+
   def initialize(value = nil, next_node = nil)
     @value = value
     @next_node = next_node
